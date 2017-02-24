614ebd0eb1baaf66f9086cd3921a0e5b26e7ebf2
srbarden <sbarden@wellesley.edu>
2017-02-24 18:54:46 -0500

    submitting pickling toolbox

--- counter.py
+++ counter.py
@@ -1,5 +1,9 @@
-""" A program that stores and updates a counter using a Python pickle file"""
-
+"""
+Sarah Barden
+Software Design Spring 2017
+Pickle Toolbox 3/24/17
+A program that stores and updates a counter using a Python pickle file"""
+import pickle
 from os.path import exists
 import sys
 from pickle import dump, load
@@ -16,7 +20,7 @@ def update_counter(file_name, reset=False):
 
     file_name: the file that stores the counter to be incremented.  If the file
     doesn't exist, a counter is created and initialized to 1.
-    reset: True if the counter in the file should be rest.
+    reset: True if the counter in the file should be reset.
     returns: the new counter value
 
     >>> update_counter('blah.txt',True)
@@ -30,9 +34,30 @@ def update_counter(file_name, reset=False):
     >>> update_counter('blah2.txt')
     2
     """
-    pass
+
+    if reset is True:
+        f = open(file_name, 'wb')
+        counter = 1
+        dump(counter, f)  # turns counter into string
+        return counter
+
+    elif reset is False:
+        if exists(file_name):
+            f = open(file_name, 'rb+')
+            counter = load(f)+1
+            f.close()
+            f = open(file_name, 'wb')
+            dump(counter, f)
+            return counter
+        else:
+            f = open(file_name, 'wb')
+            counter = 1
+            pickle.dumps(counter)  # turns counter into string
+            return counter
+
 
 if __name__ == '__main__':
+
     if len(sys.argv) < 2:
         import doctest
         doctest.testmod()
